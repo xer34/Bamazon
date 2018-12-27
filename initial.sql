e5f49747fd94c0b479bfb3d9aca2b80a58c419d8
Xer34 <experimental@gmail.com>
2018-12-26 22:56:02 -0500

    readme update

--- initial.sql
+++ initial.sql
@@ -19,13 +19,13 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Mountain Dew", "Grocery", 5.0, 1000),
 ("Pepsi", "Grocery", 5.0, 1000),
 ("Wonder Bread", "Grocery", 2.5, 500),
-("Samsung Galaxy S9", "Smartphones", 799.0, 798),
+("Samsung Galaxy S9", "Smartphones", 799.0, 3),
 ("Axe Body Spray", "Personal Care", 4.0, 8488),
 ("Old Game Cubes", "Electronics", 5.0, 1000),
 ("Chocolate Muffins", "Grocery", 2.0, 555),
-("Apple Macbooks", "Electronics", 2999.0, 98),
-("World of Warcraft Classic", "Electronics", 55.0, 9999);
-    
+("Apple Macbooks", "Electronics", 2999.0, 4),
+("World of Warcraft", "Electronics", 55.0, 9999),
+("Apple Airbuds", "Electronics", 199.0, 2);
     
     
     
